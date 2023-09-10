25f0caaad6a39428a475e4ae4aaa444b0bc18572
Josh Contreras <33960872+Snarbo@users.noreply.github.com>
2023-09-10 19:18:13 +0100

    Updated the height of the item image

--- src/Components/Countries/CountryItem.js
+++ src/Components/Countries/CountryItem.js
@@ -5,7 +5,7 @@ const CountryItem = ({ id, flag, name, population, region, capital }) => {
     <article className="country-item shadow-[0_0_7px_2px_rgba(0,0,0,0.03)]">
       <Link className="flex flex-col h-full" to={`/detail/` + id}>
         <div className="country-item-head">
-          <img className="country-item-image rounded-t-[5px] w-full" src={flag} alt={name} />
+          <img className="country-item-image rounded-t-[5px] w-full h-[160px] object-cover" src={flag} alt={name} />
         </div>
         <div className="country-item-body flex-1 rounded-b-[5px] p-6 pb-[45px]">
           <h3 className="country-item-name font-extrabold text-lg">{name}</h3>
