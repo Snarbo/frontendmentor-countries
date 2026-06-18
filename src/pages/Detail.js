535f796055085a569044520814f4445245837ce1
Josh Contreras <josh.r.contreras@gmail.com>
2026-06-18 14:37:55 +0100

    Potential fix for the detail country border link

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -103,9 +103,9 @@ const Detail = () => {
                 <h3 className="text-base font-semibold leading-6 lg:mt-1 lg:mr-4">Border Countries: </h3>
                 <ul className="flex flex-wrap mt-4 lg:flex-1 lg:mt-0 lg:mb-[-10px]">
                   {updatedCountryBorders.map((country, idx) => (
-                    <a className="mr-2.5 mb-2.5 py-2.5 px-[30px] text-xs font-light leading-none shadow-[0_0_4px_1px_rgba(0,0,0,0.1)] lg:text-sm lg:leading-none" key={idx} href={`/detail/${country.id}`}>
+                    <Link className="mr-2.5 mb-2.5 py-2.5 px-[30px] text-xs font-light leading-none shadow-[0_0_4px_1px_rgba(0,0,0,0.1)] lg:text-sm lg:leading-none" key={idx} to={`/detail/${country.id}`}>
                       {country.name}
-                    </a>
+                    </Link>
                   ))}
                 </ul>
               </div>
